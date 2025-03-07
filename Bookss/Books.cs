a562efde597a311e1adb919726fb017bb5bc7615
Petrik Diak <diak@petrik.hu>
2025-03-07 12:46:49 +0100

    Kész

--- Bookss/Books.cs
+++ Bookss/Books.cs
@@ -1,15 +1,52 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Bookss
 {
 	internal class Books
 	{
+		string title;
+		string author;
+		int pageCount;
+		int releseYear;
 
+		public Books(string title, string author, int pageCount, int releseYear)
+		{
+			this.title = title;
+			this.author = author;
+			this.pageCount = pageCount;
+			this.releseYear = releseYear;
+		}
 
+		public string Title { get => title; }
+		public string Author { get => author; }
+		public int PageCount { get => pageCount; }
+		public int ReleseYear { get => releseYear; }
 
+		public static Books GetBookByTitle(Books[] books, string title)
+		{
+			bool meglette = false;
+			foreach (var book in books)
+			{
+				if (book.Title == title)
+				{
+					Console.WriteLine($" A Cim amit keresel " + "" + title + " ");
+					Console.WriteLine("Van ilyen : ");
+                    Console.WriteLine();
+                    return book;
+				}
+			}
+			Console.WriteLine("Nincs Ilyen");
+			return null;
+		}
+
+		public void EditBookData()
+		{
+			this.title = "ZombikEditelve";
+		}
+
+		public override string? ToString()
+		{
+			return $" Cím :{title}  Írta : {Author} {pageCount}  Oldal {releseYear} Jött ki";
+		}
 	}
 }
--- Bookss/Program.cs
+++ Bookss/Program.cs
@@ -4,7 +4,32 @@
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			Books[] books = new Books[]
+			{
+				new Books("AlmaEcet", "Evelin", 300, 2015),
+				new Books("FenyoFa", "Evelin", 300, 2015),
+				new Books("KokuszGolyo", "Kis Ernő", 300, 2015),
+				new Books("Az Éjszaka", "Trap Pista", 300, 2015),
+				new Books("Locked In", "Ádám Nagy", 300, 2015)
+			};
+
+			Books b1 = books[0];
+			Console.WriteLine(b1);
+            Console.WriteLine($"A {b1} Modositva lett");
+            b1.EditBookData();
+			Console.WriteLine($"");
+			Console.WriteLine(b1);
+
+			Books foundBook = Books.GetBookByTitle(books, "Locked In");
+			if (foundBook != null)
+			{
+				Console.WriteLine($"Megtalált Könyv : {foundBook}");
+			}
+			else
+			{
+				Console.WriteLine("A Könyv Sajnos nincs meg");
+
+			}
 		}
 	}
 }
